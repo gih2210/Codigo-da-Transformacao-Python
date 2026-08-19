f02a6fe4b78e3937664b1a559b7c50969f5170a9
aluno vocacao <geovanaleitedossantosleite@gmail.com>
2026-08-19 10:40:29 -0300

    update

---
+++ modulo03/modulo03_ativ03_implem_estrutura_geovana.py
@@ -0,0 +1,10 @@
+idade = int(input("Digite sua idade: "))
+
+if idade <= 12:
+    print("Você é uma Criança.")
+elif idade <= 17:
+    print("Você é um Adolescente.")
+elif idade <= 59:
+    print("Você é um Adulto.")
+else:
+    print("Você é um Idoso.")
