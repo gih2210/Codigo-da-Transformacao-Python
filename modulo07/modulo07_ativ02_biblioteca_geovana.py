285be3760f015602c4a2bd1b3635ac6c764eea3c
aluno vocacao <geovanaleitedossantosleite@gmail.com>
2026-08-26 11:33:35 -0300

    update

--- modulo07/modulo07_ativ02_biblioteca_geovana.py
+++ modulo07/modulo07_ativ02_biblioteca_geovana.py
@@ -1,16 +1,36 @@
 from faker import Faker
 
-import utilidades
-
 fake = Faker("pt_BR")
 
+
+# UTILIDADES
+def soma(a, b):
+    return a + b
+
+
+def subtrair(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def potencializar(a, b):
+    return a ** b
+
+
+# PROGRAMA
 print("=== UTILIDADES ===")
-print("Soma:", utilidades.soma(10, 5))
-print("Subtração:", utilidades.subtrair(10, 5))
-print("Multiplicação:", utilidades.multiplicar(10, 5))
-print("Potência:", utilidades.potencializar(10, 5))
+
+print("Soma:", soma(10, 5))
+print("Subtração:", subtrair(10, 5))
+print("Multiplicação:", multiplicar(10, 5))
+print("Potência:", potencializar(10, 5))
+
 
 print("\n=== DADOS FALSOS ===")
+
 print("Nome:", fake.name())
 print("E-mail:", fake.email())
 print("Telefone:", fake.phone_number())
