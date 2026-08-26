9df630669e82fe8bea9018fab8605dbbcb5fabc1
aluno vocacao <geovanaleitedossantosleite@gmail.com>
2026-08-26 11:43:30 -0300

    update

---
+++ modulo08/modulo08_ativ03_geovana.py
@@ -0,0 +1,10 @@
+class Carro:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def __str__(self):
+        return f"Carro: {self.marca} - {self.modelo}"
+
+carro = Carro("Honda", "Civic")
+print(carro)
--- modulo08/modulo08_desafio_crie_carro_geovana copy.py
+++ modulo08/modulo08_desafio_crie_carro_geovana copy.py
@@ -1,16 +1,33 @@
-class Carro:
-    def __init__(self, marca, modelo):
-        self.marca = marca
-        self.modelo = modelo
-    def exibir_info(self):
-        return f"Marca: {self.marca}, Modelo: {self.modelo}"
-class CarroEletrico(Carro):
-    def __init__(self, marca, modelo, autonomia_bateria):
-            super().__init__(marca, modelo)
-            self.autonomia = autonomia_bateria
-    def exibir_info(self):
-        info_base = super().exibir_info()
-        return f"{info_base} | Autonomia da Bateria: {self.autonomia} km"
+class Livro:
+    def __init__(self, titulo):
+        self.titulo = titulo
+        self.emprestado = False
 
-meu_carro = CarroEletrico("BYD", "Dolphin", 600)
-print(meu_carro.exibir_info())
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+
+    def adicionar_livro(self, livro):
+        self.livros.append(livro)
+
+    def emprestar_livro(self, titulo):
+        for livro in self.livros:
+            if livro.titulo == titulo and not livro.emprestado:
+                livro.emprestado = True
+                print("Livro emprestado.")
+                return
+        print("Livro não disponível.")
+
+    def mostrar_livros(self):
+        for livro in self.livros:
+            status = "Emprestado" if livro.emprestado else "Disponível"
+            print(livro.titulo, "-", status)
+
+biblioteca = Biblioteca()
+
+biblioteca.adicionar_livro(Livro("Dom Casmurro"))
+biblioteca.adicionar_livro(Livro("O Pequeno Príncipe"))
+
+biblioteca.mostrar_livros()
+biblioteca.emprestar_livro("Dom Casmurro")
+biblioteca.mostrar_livros()
